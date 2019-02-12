8700ebf69b5fe3bd7ccf3aec5e49715a0edeb703
Shawn Ren <doggy-ren@hotmail.com>
2019-02-12 04:20:02 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -2,8 +2,7 @@
 def loop_iterator(number_of_times)
   counter = 0
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  loop
-  do
+  loop do
     counter += 1
     puts phrase
     if counter >= number_of_times
